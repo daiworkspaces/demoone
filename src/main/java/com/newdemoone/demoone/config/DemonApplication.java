c5c2907a4a576c9bb3fbd4d32abd9fcb1aa985b3
xmcb008 <xmcb008@sina.com>
2022-04-28 00:42:08 +0800

    开发了hello接口2-10

--- src/main/java/com/newdemoone/demoone/config/DemonApplication.java
+++ src/main/java/com/newdemoone/demoone/config/DemonApplication.java
@@ -1,13 +1,14 @@
-package com.newdemoone.demoone;
+package com.newdemoone.demoone.config;
 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.ComponentScan;
 
 
+@ComponentScan("com.newdemoone")
 @SpringBootApplication
 public class DemonApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(DemonApplication.class);
--- src/main/java/com/newdemoone/demoone/controller/TestController.java
+++ src/main/java/com/newdemoone/demoone/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.newdemoone.demoone.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    //@RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         return "Hello world";
     }
