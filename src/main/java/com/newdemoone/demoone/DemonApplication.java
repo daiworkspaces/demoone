4a5dcdb561d0a08c2c5efc41a0fc2671886e5d76
xmcb008 <xmcb008@sina.com>
2022-04-27 20:56:22 +0800

    优化启动日志 slf4j 2-7

--- src/main/java/com/newdemoone/demoone/DemonApplication.java
+++ src/main/java/com/newdemoone/demoone/DemonApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 
@@ -16,8 +15,11 @@ public class DemonApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemonApplication.class);
 		Environment env = app.run(args).getEnvironment();
-		LOG.info("启动成！");
-		LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+		LOG.info("启动成功！！");
+		LOG.info("地址：\thttps://127.0.0:{}",env.getProperty("server.port"));
+
+
 
 
 		SpringApplication.run(DemonApplication.class, args);
