fcc59467b13a3280f95dd939d28b11f7a98ed5a5
xmcb008 <xmcb008@sina.com>
2022-04-27 20:07:18 +0800

    修改启动文案2-6

--- src/main/java/com/newdemoone/demoone/DemonApplication.java
+++ src/main/java/com/newdemoone/demoone/DemonApplication.java
@@ -1,12 +1,25 @@
 package com.newdemoone.demoone;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class DemonApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(DemonApplication.class);
 
 	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(DemonApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成！");
+		LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+
 		SpringApplication.run(DemonApplication.class, args);
 	}
 
