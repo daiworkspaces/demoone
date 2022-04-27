fff0fb00d7004eccb444da57737aa72131a65ba3
xmcb008 <xmcb008@sina.com>
2022-04-27 19:31:14 +0800

    增加logback-spring文件

--- src/main/java/com/newdemoone/demoone/DemonApplication.java
+++ src/main/java/com/newdemoone/demoone/DemonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemooneApplication {
+public class DemonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemooneApplication.class, args);
+		SpringApplication.run(DemonApplication.class, args);
 	}
 
 }
